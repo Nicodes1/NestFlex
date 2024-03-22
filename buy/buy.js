2bf497839b16936efef6e9507987f476c05b2cad
Nicodes1 <nichov01@gmail.com>
2024-03-21 23:57:30 -0600

    added to buy, map and stuff

---
+++ buy/buy.js
@@ -0,0 +1,2 @@
+const location1 = localStorage.getItem('location2');
+document.getElementById('displayLocation').textContent = location1;
--- home/home.js
+++ home/home.js
@@ -1,1 +1,10 @@
-// don't tell me that you model if you ain't been in Vogue
+const form = document.getElementById('form');
+const loc = document.getElementsById('search-bar');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const location = loc.value;
+    localStorage.setItem('location2', location);
+    window.location.href = "/buy/buy.html";
+})
