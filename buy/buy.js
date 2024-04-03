3de548b6e442eeb2437da8dfb7b0cfe5ab564022
Nicodes1 <nichov01@gmail.com>
2024-04-03 13:22:51 -0600

    filter popup

---
+++ buy/buy.js
@@ -0,0 +1,10 @@
+let popup = document.getElementById("popup");
+function openFilter(){
+    popup.classList.add("open-popup");
+}
+function closeFilter(){
+    popup.classList.remove("open-popup")
+}
+function changeListings(){
+    popup.classList.remove("open-popup")
+}
