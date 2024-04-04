213009f3ca827cc0bc32e50cbe2c38ad551ced7a
luke.nodwell <luke.nodwell@ucalgary.ca>
2024-04-04 15:21:09 -0600

    Added Create account stuff

--- help/help.js
+++ help/help.js
@@ -1,12 +1,41 @@
 let signIn = document.getElementById("signIn");
-function openSignUp(){
+function openSignUp() {
     signIn.classList.add("open-signIn");
     document.body.style.overflow = 'hidden'; // Prevent scrolling
+
+    // Show everything for sign in
+    var forgotPass = document.getElementById('forgotPass');
+    forgotPass.classList.remove('hidden');
+
+    var signInB = document.getElementById('signInB');
+    signInB.classList.remove('hidden');
+
+
+    // Hide stuff for create account
+    var conPass = document.getElementById('conPass-bar');
+    conPass.classList.add('hidden');
+    var conPassTxt = document.getElementById('conPassTxt');
+    conPassTxt.classList.add('hidden');
 }
-function closeSignUp(){
+function closeSignUp() {
     signIn.classList.remove("open-signIn")
     document.body.style.overflow = 'auto'; // Allow scrolling
 }
-function changeListings(){
+function changeListings() {
     signIn.classList.remove("open-signIn")
 }
+
+document.getElementById('createAccountButton').addEventListener('click', function () {
+    // Hide stuff for create account
+    var forgotPass = document.getElementById('forgotPass');
+    var signIn = document.getElementById('signInB');
+
+    forgotPass.classList.add('hidden');
+    signIn.classList.add('hidden');
+
+    // Show stuff for create account
+    var conPass = document.getElementById('conPass-bar');
+    conPass.classList.remove('hidden');
+    var conPassTxt = document.getElementById('conPassTxt');
+    conPassTxt.classList.remove('hidden');
+});
