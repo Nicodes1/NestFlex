3dd9b4e67c72791689d59428b7f776873c1601bb
Luke Nodwell <luke.nodwell@ucalgary.ca>
2024-04-04 23:35:16 -0600

    Sign in and create account finished including logic

--- help/help.js
+++ help/help.js
@@ -1,4 +1,5 @@
 let signIn = document.getElementById("signIn");
+let createCounter = 0;
 function openSignUp() {
     signIn.classList.add("open-signIn");
     document.body.style.overflow = 'hidden'; // Prevent scrolling
@@ -14,8 +15,25 @@ function openSignUp() {
     // Hide stuff for create account
     var conPass = document.getElementById('conPass-bar');
     conPass.classList.add('hidden');
+
     var conPassTxt = document.getElementById('conPassTxt');
     conPassTxt.classList.add('hidden');
+
+    // Hide incorrect Info
+    var wrongInfo = document.getElementById('wrongInfo');
+    wrongInfo.classList.add('hidden');
+
+    // Hide Forgot pass
+    var resetEmail = document.getElementById('resetEmail');
+    resetEmail.classList.add('hidden');
+
+    // Hide Account Created message
+    var accountCreated = document.getElementById('accountCreated');
+    accountCreated.classList.add('hidden');
+
+    // Hide account created error message
+    var accountError = document.getElementById('accountError');
+    accountError.classList.add('hidden');
 }
 function closeSignUp() {
     signIn.classList.remove("open-signIn")
@@ -29,7 +47,7 @@ document.getElementById('createAccountButton').addEventListener('click', functio
     // Hide stuff for create account
     var forgotPass = document.getElementById('forgotPass');
     forgotPass.classList.add('hidden');
-    
+
     var signIn = document.getElementById('signInB');
     signIn.classList.add('hidden');
 
@@ -39,4 +57,58 @@ document.getElementById('createAccountButton').addEventListener('click', functio
 
     var conPassTxt = document.getElementById('conPassTxt');
     conPassTxt.classList.remove('hidden');
+
+    var wrongInfo = document.getElementById('wrongInfo');
+    wrongInfo.classList.add('hidden');
+
+    var resetEmail = document.getElementById('resetEmail');
+    resetEmail.classList.add('hidden');
+
+    var email = document.getElementById("email-bar").value;
+    var emailLength = email.length;
+    var pass = document.getElementById("pass-bar").value;
+    var passLength = pass.length;
+    var confirmPass = document.getElementById("conPass-bar").value;
+
+    if (createCounter > 0 && pass == confirmPass && emailLength > 0 && passLength > 0) {
+        // Show Account Created message
+        var accountCreated = document.getElementById('accountCreated');
+        accountCreated.classList.remove('hidden');
+
+        // Hide Account Created Error message
+        var accountError = document.getElementById('accountError');
+        accountError.classList.add('hidden');
+    } else if (createCounter > 0) {
+        // Show Account Created Error message
+        var accountError = document.getElementById('accountError');
+        accountError.classList.remove('hidden');
+    }
+    createCounter++;
 });
+
+function login() {
+    // Hide Forgot pass
+    var resetEmail = document.getElementById('resetEmail');
+    resetEmail.classList.add('hidden');
+
+    var email = document.getElementById("email-bar").value;
+    var pass = document.getElementById("pass-bar").value;
+
+    if (email == 'person@nestflex.ca' && pass == 'pass') {
+        window.location.href = '/profile/profile.html';
+    } else {
+        // Show incorrect Info
+        var wrongInfo = document.getElementById('wrongInfo');
+        wrongInfo.classList.remove('hidden');
+    }
+}
+
+function resetPass() {
+    // Hide incorrect Info
+    var wrongInfo = document.getElementById('wrongInfo');
+    wrongInfo.classList.add('hidden');
+
+    // Show reset pass
+    var resetEmail = document.getElementById('resetEmail');
+    resetEmail.classList.remove('hidden');
+}
