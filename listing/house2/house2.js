f641ec3b270a2f4242ba3775905bb0b7655a3d59
Nicodes1 <nichov01@gmail.com>
2024-04-06 23:52:59 -0600

    contact pop up started

---
+++ listing/house2/house2.js
@@ -0,0 +1,7 @@
+let popup = document.getElementById("popup");
+function openFilter(){
+    popup.classList.add("open-popup");
+}
+function closeFilter(){
+    popup.classList.remove("open-popup")
+}
