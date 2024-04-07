354361ff89249f5e19dc0b783e35cc96939f45ba
Nicodes1 <nichov01@gmail.com>
2024-04-07 13:04:11 -0600

    contact done

--- listing/house2/house2.js
+++ listing/house2/house2.js
@@ -5,3 +5,24 @@ function openFilter(){
 function closeFilter(){
     popup.classList.remove("open-popup")
 }
+let confirm = document.getElementById("confirm");
+function sent(){
+    let confirm = document.getElementById("confirm");
+    confirm.classList.add("open-confirm");
+}
+
+function yes(){
+    let confirm = document.getElementById("confirm");
+    popup.classList.remove("open-popup");
+    confirm.classList.remove("open-confirm");
+    let confirmed = document.getElementById("confirmed");
+    confirmed.classList.add("open-confirmed");
+}
+function no(){
+    let confirm = document.getElementById("confirm");
+    confirm.classList.remove("open-confirm");
+}
+function closeConfirmed(){
+    let confirmed = document.getElementById("confirmed");
+    confirmed.classList.remove("open-confirmed")
+}
