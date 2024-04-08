2083924197d47ea77b849f9e1a5c311d35c5d58a
Luke Nodwell <luke.nodwell@ucalgary.ca>
2024-04-07 21:18:09 -0600

    Finished Posted popup

--- sell/sell.js
+++ sell/sell.js
@@ -1,3 +1,17 @@
+let posted = document.getElementById("post");
+function openPosted() {
+
+  posted.classList.add("open-posted");
+  document.body.style.overflow = 'hidden'; // Prevent scrolling
+}
+
+function closePosted() {
+  posted.classList.remove("open-posted")
+  document.body.style.overflow = 'auto'; // Allow scrolling
+}
+
+
+// Picture upload stuff
 document.getElementById('fileInput').addEventListener('change', function() {
         var file = this.files[0];
         var reader = new FileReader();
